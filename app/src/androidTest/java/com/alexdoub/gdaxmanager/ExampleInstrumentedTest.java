3a56d6d35e9e73b39cb9be8b0dbabd4cd59726c2
Alex Doub <alexdoub1990@gmail.com>
2017-11-05 22:39:49 -0700

    More package updates. Updated APK again

--- app/src/androidTest/java/com/alexdoub/gdaxmanager/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/alexdoub/gdaxmanager/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.alex.bitcoinmanager", appContext.getPackageName());
+        assertEquals("com.alexdoub.bitcoinmanager", appContext.getPackageName());
     }
 }
